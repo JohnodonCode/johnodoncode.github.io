f2c4ed854b55743da9d4e156e2d87c3787020cc5
John Bostick <jbost0114@gmail.com>
2019-05-16 11:36:21 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<DOCTYPE! html>
+<html>
+<head>
+<title> JohnodonCode on Github </title>
+<style>
+body {
+font-family: sans
+}
+
+</style>
+</head>
+<body>
+<h3> Hello! </h3>
+
+
+
+
+</body>
+</html>
